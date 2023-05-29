91841b49206ff124309a2219e58d97af6e010cf6
BlizzardBlast <64456154+BlizzardBlast@users.noreply.github.com>
2023-05-29 11:24:36 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/portfolio-1",
   plugins: [react()],
+  logLevel: "info", //Default "info"
+  envDir: ".", // Default "."
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
+  clearScreen: false, // Default false
+  envPrefix: "VITE_", // Default "VITE_"
+  css: {
+    devSourcemap: true,
+  },
 });
